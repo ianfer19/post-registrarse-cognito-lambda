789c2307aca4e11c5b723539bef1af2bbb84a528
ianfer19 <ifernandezalvarez@correo.unicordoba.edu.co>
2025-12-11 03:39:37 -0500

    feat: Implement Cognito repository for user signup functionality, including temporary password setting and confirmation email.

--- infrastructure/cognito_repository.py
+++ infrastructure/cognito_repository.py
@@ -1,8 +1,8 @@
 import boto3
 from botocore.exceptions import ClientError
 from uuid import uuid4
-from src.domain.exceptions import CognitoError
-from src.utils.logger import get_logger
+from domain.exceptions import CognitoError
+from utils.logger import get_logger
 
 logger = get_logger(__name__)
 
